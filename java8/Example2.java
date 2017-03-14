94a3c32e2f513aacda48f693755ea332870b5f36
NiuYongjie <1054602234@qq.com>
2017-03-14 21:42:57 +0800

    添加第二个小型程序示例,演示变量的声明,变量的赋值,内置类型的值的输出

---
+++ java8/Example2.java
@@ -0,0 +1,20 @@
+/*
+  Here is another short example.
+  Call this file 'Example2.java'
+*/
+
+class Example2{
+  public static void main(String args[]){
+    int num; // this declares a variable called num.
+
+    num = 100; // this assigns num the value 100.
+
+    System.out.println("This is the num: " + num);
+
+    num = num * 2;
+   
+    System.out.print("The value of num * 2 is: ");
+
+    System.out.println( num );// print() 和 println()方法能够输出所有Java内置类型的值
+  }
+}
