1d0ab8321a163a7afc07fa743d66311ab75cfd9b
niuyongjie <1054602234@qq.com>
2017-08-30 06:29:18 +0800

    增加位移运算实例

---
+++ java8/MultByTwo.java
@@ -0,0 +1,13 @@
+/**
+ * 左移，相当于将原始值乘以2，注意高阶位的特殊情况
+ */
+class MultByTwo{
+	public static void main(String[] args){
+		int num = 0xFFFFFFE;
+
+		for(int i = 0; i < 4; i++){
+			num = num << 1;
+			System.out.println(num);
+		}
+	}
+}
