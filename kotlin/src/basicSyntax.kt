10e29e8204346799aee4f8f56b5422567ca5dc7a
niuyongjie <1054602234@qq.com>
2017-10-13 14:11:11 +0800

    增加kotlin模块

---
+++ kotlin/src/basicSyntax.kt
@@ -0,0 +1,12 @@
+package my.demo
+
+import java.util.*
+
+fun main(args: Array<String>) {
+    println("sum of 3 and 5 is ")
+    println(sum(3, 5))
+}
+
+fun sum(a: Int, b: Int): Int {
+    return a + b
+}
