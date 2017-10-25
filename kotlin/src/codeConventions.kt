7ecd48a86aad04c97189f864581ce6336c93e962
yongjie niu <1054602234@qq.com>
2017-10-25 14:05:56 +0800

    增加语法规则练习

--- kotlin/src/codeConventions.kt
+++ kotlin/src/codeConventions.kt
@@ -1,1 +1,23 @@
 package my.demo
+
+import org.omg.CORBA.Object
+
+fun main(args: Array<String>) {
+    /*
+        命名规则
+        1. 采用驼峰命名法(不包括下划线)
+        2. 类型第一个字母大写
+        3. 方法和属性名第一个字母小写
+        4. 使用4个空格缩进
+        5. public修饰的方法应当包含文档
+     */
+    }
+
+/*
+    colon冒号分隔规则:
+    类型 : 类型  -> 前后都有空格分隔
+    实例: 类型   -> 后边用一个都好分隔
+ */
+interface Foo<out T : Any> : Object{
+    fun foo(a: Int): T
+}
