e2912d549581f59df7f6531f45442b8b6ca480ce
yongjie niu <1054602234@qq.com>
2017-10-25 15:23:53 +0800

    新增练习文件:基本类型

---
+++ kotlin/src/basicType.kt
@@ -0,0 +1,23 @@
+package my.demo
+
+/*
+    kotlin中,万物皆对象
+ */
+fun main(args: Array<String>) {
+
+}
+
+/*
+    Numbers--数值型
+    与Java中数值型类似,但没有隐含的类型转换
+    Type |Bit Width
+    Double| 64
+    Float| 32
+    Long| 64
+    Int| 32
+    Short| 16
+    Byte| 8
+ */
+fun numbersExpression(){
+
+}
