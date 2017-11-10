b56cbd7ec2d5df3b6afff3fe42824454133e88db
yongjie niu <1054602234@qq.com>
2017-11-10 14:42:04 +0800

    增加包导入示例

---
+++ kotlin/src/packageDemo.kt
@@ -0,0 +1,32 @@
+package my.demo
+
+/*
+    Kotlin的源码文件以报名开始,方法和类的全名为包名+方法/类名
+
+    每个Kotlin的源代码文件都会默认导入一下包
+    — kotlin.*
+    — kotlin.annotation.*
+    — kotlin.collections.*
+    — kotlin.comparisons.* (since 1.1)
+    — kotlin.io.*
+    — kotlin.ranges.*
+    — kotlin.sequences.*
+    — kotlin.text.*
+
+    Kotlin可以转换成java和js代码,必须引入相应平台的包
+    — JVM:
+    — java.lang.*
+    — kotlin.jvm.*
+    — JS:
+    — kotlin.js.*
+
+    引入包的三种形式
+    import foo.Bar
+    import foo.*
+    import foo.bar.Bar as bBar //起别名
+
+    Kotlin中没有静态导入语法
+ */
+fun main(args: Array<String>) {
+
+}
