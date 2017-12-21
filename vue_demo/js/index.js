c14d9016be5637bf7b358b4e6f6507be2a4d63c7
niu yongjie <1054602234@qq.com>
2017-12-22 07:36:05 +0800

    create empty work

---
+++ vue_demo/js/index.js
@@ -0,0 +1,12 @@
+var vm = new Vue({
+    el: '#app',
+    data: {
+        show: true,
+        customStyle: {
+            'border-color': 'red',
+            'border-style': 'solid',
+            width: '200px'
+        }
+    }
+});
+
